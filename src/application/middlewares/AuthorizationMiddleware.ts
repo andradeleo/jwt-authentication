52b5ee3558158bbad8d04d11210ba2b920bc01c4
Leonardo <contato.leoo0@gmail.com>
2024-11-30 13:52:57 -0300

    feat: add AuthorizationMiddleware

--- src/application/middlewares/AuthorizationMiddleware.ts
+++ src/application/middlewares/AuthorizationMiddleware.ts
@@ -2,7 +2,29 @@ import { IData, IMiddleware, IResponse } from "../interfaces/IMiddleware";
 import { IRequest } from "../interfaces/IRequest";
 
 export class AuthorizationMiddleware implements IMiddleware {
-  async handle({ headers }: IRequest): Promise<IResponse | IData> {
-    throw new Error();
+  constructor(private readonly allowedRoles: string[]) {}
+
+  async handle({ account }: IRequest): Promise<IResponse | IData> {
+    if (!account) {
+      return {
+        statusCode: 403,
+        body: {
+          error: "Access Denied",
+        },
+      };
+    }
+
+    if (!this.allowedRoles.includes(account.role)) {
+      return {
+        statusCode: 403,
+        body: {
+          error: "Access Denied",
+        },
+      };
+    }
+
+    return {
+      data: {},
+    };
   }
 }
